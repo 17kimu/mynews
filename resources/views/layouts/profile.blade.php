1cc3a0fc6941e4ce61e1e07d97f1573f7ede2c5c
sayaka <sayaka0530kimura@yahoo.co.jp>
2019-06-22 15:00:52 +0900

    login2

--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -42,26 +42,28 @@
 
                      @else
                           <li class="nav-item dropdown">
-                              <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                              data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                               <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
 
-                               {{ Auth::user()->name }} <span class="caret"></span>
-                             </a>
+                                   {{ Auth::user()->name }} <span class="caret"></span>
+                               </a>
 
-                             <a class="dropdown-item" href="{{ route('logout') }}"
+                             <div class ="dropdown-menu" aria-labelledby="navbalDropdown">
+
+                               <a class="dropdown-item" href="{{ route('logout') }}"
                                   onclick="event.preventDefault();
-                                       document.getElementBuId('logout-form').submit();">
+                                              document.getElementById('logout-form').submit();">
 
                                          {{ __('Logout') }}
-                            </a>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                               </a>
+                                   <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
 
                                    ＠csrf
-                            </form>
-                        </div>
-                    </li>
+                                 </form>
+                               </div>
+                        </li>
                     @endguest
-                                                               
+
                    </ul>
 
                  </div>
--- routes/web.php
+++ routes/web.php
@@ -37,8 +37,7 @@ Route ::group(['prefix' => 'admin'], function() {
      Route::get('profile/create', 'Admin\ProfileController@add' )->middleware('auth');
    });
 
-Route::group(['prefix => 'admin'], function() {
-
+Route::group(['prefix' => 'admin'], function() {
      Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
    });
 
